d7826863b64fb7adbe6eb6140eebf62db74b6f7e
tanya280924 <vermatanya417@gmail.com>
2020-08-26 18:47:16 +0530

    initial commit

---
+++ chat application/scriptclient.js
@@ -0,0 +1,39 @@
+const socket=io('http://localhost:8000');
+const form= document.getElementById('sendcontainer');
+const messageinput=document.getElementById('messageinp');
+const messagecontainer=document.querySelector('.container');
+var audio=new Audio('ting.mp3');
+ const append=(message,position)=>{
+    const messageelement=document.createElement('div');
+    messageelement.innerText=message;
+    messageelement.classList.add('message');
+    messageelement.classList.add(position);
+    messagecontainer.append(messageelement);
+    if (position=='left'){
+         audio.play();
+    }
+}
+
+const name=prompt('Enter your name to join the chat');
+socket.emit('new-user-joined',name);
+socket.on('user-joined',name=>{
+    append(`${name} joined the chat `,'right')
+    
+});
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const message=messageinput.value;
+    messageinput.value="";
+    append(`You:${message}`,'right');
+    socket.emit('send',message);
+    
+
+
+})
+socket.on('receive',data=>{
+    append(`${data.name}:${data.message}`,'left')
+    
+});
+socket.on('gone',name=>{
+    append(`${name} left the chat`,'right')
+})
